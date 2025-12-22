41f6f1cb7e0ed67ded38eff7e507d74424a11fbb
naehyun kim <scott2535@naver.com>
2025-12-22 23:02:02 +0900

    feat: refactor code in App.vue

--- front/library/src/App.vue
+++ front/library/src/App.vue
@@ -1,21 +1,10 @@
 <template>
-   <!-- <LandingView /> -->
-    <!-- <SignUp /> -->
-    <!-- <MainView /> -->
-     <!-- <BookDetailView /> -->
-    <ProfileView />
+  <NavBar />
+  <RouterView />
 </template>
 
 <script setup>
-
-import LandingView from './views/LandingView.vue';
-import SignUp from './views/SignUp.vue';
-import MainView from './views/MainView.vue';
-import BookDetailView from './views/BookDetailView.vue';
-import ProfileView from './views/ProfileView.vue';
-
+import { RouterView, RouterLink } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
-<style scoped>
-
-
-</style>
+<style scoped></style>
