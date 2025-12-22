ed57cca07dcc3f05a41e3907848eba89364d30db
naehyun kim <scott2535@naver.com>
2025-12-22 23:02:56 +0900

    feat: implement router

---
+++ front/library/src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import LandingView from "@/views/LandingView.vue";
+import MainView from "@/views/MainView.vue";
+import SearchResultsView from "@/views/SearchResultsView.vue";
+import SignUpView from "@/views/SignUpView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+import ProfileInfo from "@/components/ProfileInfo.vue";
+import ProfileUpdate from "@/components/ProfileUpdate.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "landing",
+      component: LandingView,
+    },
+    {
+      path: "/main",
+      name: "main",
+      component: MainView,
+    },
+    {
+      path: "/main/:search",
+      name: "searchResult",
+      component: SearchResultsView,
+    },
+    {
+      path: "/signup",
+      name: "signUp",
+      component: SignUpView,
+    },
+    {
+      path: "/main/profile",
+      component: ProfileView,
+      children: [
+        {
+          path: "",
+          name: "profileDetail",
+          component: ProfileInfo,
+        },
+        {
+          path: "update",
+          name: "profileUpdate",
+          component: ProfileUpdate,
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
