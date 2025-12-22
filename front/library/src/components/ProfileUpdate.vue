9e6b668181d581c968ea5f991797eee0d0eef801
naehyun kim <scott2535@naver.com>
2025-12-22 21:59:35 +0900

    feat: implement profile update with ProfileUpdate in ProfileUpdate.vue

---
+++ front/library/src/components/ProfileUpdate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex items-start gap-4">
+    <div
+      class="w-12 h-12 bg-blue-100 rounded-lg flex items-center justify-center flex-shrink-0"
+    >
+      <img :src="imgSrc" :alt="imgAlt" class="w-6 h-6" />
+    </div>
+
+    <div class="flex-1">
+      <label class="block text-sm text-gray-600">{{ field }}</label>
+      <BaseInput class="text-gray-800 py-2" :value="value" type="text" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import BaseInput from "./BaseInput.vue";
+
+const { imgSrc, imgAlt, field, value } = defineProps({
+  imgSrc: String,
+  imgAlt: String,
+  field: String,
+  value: String,
+});
+</script>
+
+<style scoped></style>
