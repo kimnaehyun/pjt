59abec8c72069c87ed5cd4a14a6768fa70c64f5d
naehyun kim <scott2535@naver.com>
2025-12-22 21:41:09 +0900

    refactor: remove InfoRow and integrate logic into ProfileInfo

--- front/library/src/components/ProfileInfo.vue
+++ front/library/src/components/ProfileInfo.vue
@@ -1,38 +1,25 @@
 <template>
-   <div class="flex items-start gap-4 ">
+  <div class="flex items-start gap-4">
+    <div
+      class="w-12 h-12 bg-blue-100 rounded-lg flex items-center justify-center flex-shrink-0"
+    >
+      <img :src="imgSrc" :alt="imgAlt" class="w-6 h-6" />
+    </div>
 
-  <div class="w-12 h-12 bg-blue-100 rounded-lg flex items-center justify-center flex-shrink-0">
-
-    <img :src="imgSrc" :alt="imgAlt" class="w-6 h-6">
+    <div class="flex-1">
+      <label class="block text-sm text-gray-600">{{ field }}</label>
+      <p class="text-gray-800 py-2">{{ value }}</p>
+    </div>
   </div>
-
-  <div class="flex-1">
-    <label class="block text-sm text-gray-600 ">{{ field }}</label>
-    <component :is="componentToShow" v-bind="componentProps" />
-  </div>
-</div>
-
 </template>
 
 <script setup>
-import InfoRow from './InfoRow.vue';
-import BaseInput from './BaseInput.vue';
-
-const { imgSrc, imgAlt, field, value, type } = defineProps({
+const { imgSrc, imgAlt, field, value } = defineProps({
   imgSrc: String,
   imgAlt: String,
   field: String,
   value: String,
-  type: {
-    type: String,
-    default: 'info'
-  }
 });
-
-const componentToShow = type === 'input' ? BaseInput : InfoRow;
-const componentProps = type === 'input' ? { modelValue: value } : { value };          
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
