e6f098d58d70b60c6b0f7be4f0d43ae1b1d8d186
kimnaehyun <rlasogus1@gmail.com>
2025-12-23 09:21:24 +0900

    feat: connect BaseInput v-model and implement search routing

--- front/library/src/views/MainView.vue
+++ front/library/src/views/MainView.vue
@@ -4,9 +4,10 @@
     <!-- 검색 섹션 -->
     <div class="mb-12">
       <h1 class="text-center mb-8 text-gray-800 text-2xl font-semibold">도서를 검색해보세요</h1>
-     <form class="mb-8">
+     <form class="mb-8" @submit.prevent="search">
   <div class="flex items-center gap-2">
-    <BaseInput 
+    <BaseInput
+      v-model="keyword" 
       type="text" 
       placeholder="책 제목, 저자, 카테고리로 검색" 
       class=" border-gray-300 focus:border-blue-600 focus:outline-none shadow-lg"
@@ -14,7 +15,7 @@
     <BaseButton 
       type="submit" 
       class="bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors px-6 py-2 flex-1" 
-      value="검색" 
+      value="검색"
     />
   </div>
 </form>
@@ -35,6 +36,25 @@
 import BaseButton from '@/components/BaseButton.vue';
 import BaseInput from '@/components/BaseInput.vue';
 import BookCard from '@/components/BookCard.vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const keyword = ref('')
+
+
+const search = () => {
+  // TODO: 검색 기능 구현
+  console.log(keyword);
+  
+
+  router.push({
+  name: 'searchResult',
+  params: {
+    search: keyword.value,
+  },
+});
+}
 
 const books = [
   {
