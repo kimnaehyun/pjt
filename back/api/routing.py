3d67145b965f8c1ab9169031ce760c2f52de0f69
kimnaehyun <rlasogus1@gmail.com>
2025-12-24 12:45:00 +0900

    test: add code

--- back/api/routing.py
+++ back/api/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/books/(?P<book_id>\d+)/$', consumers.ReviewConsumer.as_asgi()),
+    re_path(r'ws/books/(?P<book_id>\d+)/$', consumers.BookConsumer.as_asgi()),
 ]
